18fe4b8fd3ab60bda0af25defe571f2e387ac647
bambangsangdewa <bambangaja2004@gmail.com>
2023-11-16 10:57:30 +0700

    first commit

---
+++ tampilan2.js
@@ -0,0 +1,10 @@
+
+const pertanyaan = document.getElementsByClassName('selesai');
+  document.addEventListener('click' ,function(pertanyaan)
+    {
+     const nama = alert('Are You sure Submite?')
+    })
+
+
+
+    
